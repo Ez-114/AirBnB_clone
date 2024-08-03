451d07bf81e3c1480a068224ee8e6c27fa67d395
Ez-114 <morganezz226@gmail.com>
2024-08-03 16:21:56 +0300

    added BaseModel class in /models/base_model.py and its test cases in /tests/test_models/test_base_model.py

---
+++ models/base_model.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+"""
+models/base_models.py
+
+This module defines the BaseModel class in the models package
+that serves as a base class for all other models.
+
+The BaseModel class provides common attributes and methods for other models,
+including:
+    - id: A unique identifier for each instance.
+    - created_at: The timestamp when an instance is created.
+    - updated_at: The timestamp when an instance is updated.
+    - __str__: A method that provides a string representation of the instance.
+    - save: A method to update the 'updated_at' timestamp.
+    - to_dict: A method to generate a dictionary representation
+                of the instance.
+
+The BaseModel class uses the UUID python package to give its instances their
+unique ids. It also uses the datetime package to give each instance
+its timestamps.
+"""
+
+from datetime import datetime
+import uuid
+
+
+class BaseModel:
+    """
+    BaseModel class.
+
+    The parent of all model classes. It defines all common
+    attributes and methods for other model classes.
+    """
+
+    def __init__(self):
+        """
+        BaseModel.__init__() Initialization method.
+
+        It initializes a new BaseModel instance
+        giving it its unique identifiers.
+        """
+
+        id = str(uuid.uuid4())
+        created_at = datetime.now()
+
+        # Consider that the instace being constructed so create and update
+        # dates are equal
+        updated_at = datetime.now()
+
+    def save(self):
+        """
+        BaseModel.save() Instance method.
+
+        Updates the public instance attribute `updated_at` with the current
+        new timestamp.
+        """
+
+        updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        BaseModel.to_dict() Instance method.
+
+        Creates the dictionary representation of the BaseModel instance,
+        including:
+            - all instance attributes.
+            - the class name.
+            - its timestamps in iso format
+        
+        Returns:
+            dict: The created dictionary representation.
+        """
+
+        instance_dict = self.__dict__.copy()
+        instance_dict['__class__'] = self.__class__.__name__
+        instance_dict['created_at'] = self.created_at.isoformat()
+        instance_dict['updated_at'] = self.updated_at.isoformat()
+
+        return instance_dict
+
+    def __str__(self):
+        """
+        BaseModel.__str__() Instance method.
+
+        Creates the string representation of the BaseModel instance.
+
+        Returns:
+            str: the created string representation.
+        """
+
+        return "[{}] ({}) {}".format(
+                        self.__class__.__name__,
+                        self.id,
+                        self.__dict__
+                    )
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+"""
+test_base_model.py
+
+This module define tests that will help validate that BaseModel class
+functions as it is intended to.
+
+The module achives this by the help of the unittest module.
+"""
+from datetime import datetime
+from models.base_model import BaseModel
+import unittest
+import uuid
+
+
+class TestBaseModel(unittest.TestCase):
+    """
+    TestBaseModel class.
+
+    This class contain all possible test cases that ensure
+    the BaseModel class is working correctly.
+    """
+
+    def setUp(self):
+        """
+        setUp method.
+
+        sets up instances of the BaseModel class.
+        """
+        base1 = BaseModel()
+        base2 = BaseModel()
+
+    def test_unique_id(self):
+        """
+        test_unique_id test case.
+
+        This test case tests wither the assigned instance id
+        differes from other instance's id or not
+        """
+        self.assertIsInstance(base1.id, uuid.UUID, msg="Error: Invalid uuid")
+        self.assertIsInstance(base2.id, uuid.UUID, msg="Error: Invalid uuid")
+        self.assertNotEqual(base1.id, base2.id, msg="Error: Ids are the same")
+
+    def test_created_at(self):
+        """
+        test_created_at test case.
+
+        This test case tests wither the assigned instance timestamp is created
+        correctly and both creation and update times are equal.
+        """
+        err_msg = "Error: Invalid timestamp"
+        self.assertIsInstance(base1.created_at, datetime, err_msg)
+        self.assertIsInstance(base1.updated_at, datetime, err_msg)
+        self.assertEqual(base1.created_at, base1.updated_at, err_msg)
+
+    def test_update_timestamp(self):
+        """
+        test_update_timestamp test case.
+
+        This test case tests wither the assigned instance timpestamp is updated
+        when the save method is called or not. Also, tests if the updated
+        timestamp is greater than the old timestamp
+        """
+        old_time = base1.updated_at
+        base1.save()
+        self.assertNotEqual(old_time, base1.updated_at)
+        self.assertTrue(base1.updated_at > old_time)
+
+    def test_dictionary_repr(self):
+        """
+        test_dictionary_repr test case.
+
+        This test case tests wither the dictionary representation
+        of the instance by validating if it is a dictionary data type and
+        contain all the required attributes:
+            - class name
+            - id
+            - create date
+            - update date
+        """
+        bm_dict = base1.to_dict()
+        self.assertIsInstance(bm_dict, dict)
+        self.assertEqual(bm_dict['id'], str(base1.id))
+        self.assertEqual(bm_dict['__class__'], 'BaseModel')
+        self.assertEqual(bm_dict['created_at'], base1.created_at.isoformat())
+        self.assertEqual(bm_dict['updated_at'], base1.updated_at.isoformat())
+
+    def test_string_repr(self):
+        """
+        test_string_repr test case.
+
+        This test case tests the string representation of the instance and
+        check if it contains the required info:
+            e.g. [<class name>] (<self.id>) <self.__dict__>
+        """
+        expected_str = "[BaseModel] ({}) {}".format(base1.id, base1.__dict__)
+        self.assertEqual(str(base1), expected_str)
+
+
+if __name__ == "__main__":
+    unittest.main()
