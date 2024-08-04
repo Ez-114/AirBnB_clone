7dbac89a917517149fd1cde973aa998b34b70737
Ez-114 <morganezz226@gmail.com>
2024-08-04 23:46:19 +0300

    styling the code

--- console.py
+++ console.py
@@ -10,24 +10,26 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     """
     HBNBCommand class.
-    
+
     Implements the command interpreter of the AirBNB project.
     """
     prompt = "(hbnb) "
-    
+
     def do_EOF(self, line):
         """Exit the interpreter using the EOF Flag.
         """
+        print()
         return True
-    
+
     def do_quit(self, line):
         """Exit the interpreter using the quit command.
         """
         return True
-    
+
     def emptyline(self):
         """Do nothing on empty input line."""
         pass
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
