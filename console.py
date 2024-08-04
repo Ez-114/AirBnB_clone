c5b6ee2cc94eeac177103071220a72728642a06e
Ez-114 <morganezz226@gmail.com>
2024-08-04 23:31:06 +0300

    added interpreter entry point

---
+++ console.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+The console module.
+
+Contains the entry point of the command interpreter.
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    HBNBCommand class.
+    
+    Implements the command interpreter of the AirBNB project.
+    """
+    prompt = "(hbnb) "
+    
+    def do_EOF(self, line):
+        """Exit the interpreter using the EOF Flag.
+        """
+        return True
+    
+    def do_quit(self, line):
+        """Exit the interpreter using the quit command.
+        """
+        return True
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
