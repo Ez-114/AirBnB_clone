a33c7829738646fedb253dbf300c349028826029
Ez-114 <morganezz226@gmail.com>
2024-09-01 16:58:10 +0300

    [fix] User Model init

--- models/user.py
+++ models/user.py
@@ -10,7 +10,7 @@ from models.base_model import BaseModel
 class User(BaseModel):
     """User model class."""
 
-    email = None
-    password = None
-    first_name = None
-    last_name = None
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
